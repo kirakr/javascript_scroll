3f1737745fe9c0fa52f67a31da3ad13d9327af8e
kirakr <82437235+kirakr@users.noreply.github.com>
2023-10-30 15:13:03 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//一度表示したら動作を止める
+const show = (entries, obs) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting){
+        const keyframes = {
+          opacity: [0, 1],
+          translate: ['-500px 0',0]
+        }
+        entry.target.animate(keyframes,1200);
+  
+        //一度表示されたら止める
+        obs.unobserve(entry.target);
+      }
+    });
+  }
+  
+  const observer = new IntersectionObserver(show);
+  
+  //監視対象
+  const images = document.querySelectorAll('.img');
+  images.forEach(img =>{
+    observer.observe(img);
+  });
